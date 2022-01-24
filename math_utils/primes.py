db686d56acba3990736e40f39232d1f7dc596f13
zy3120 <zijieyuan@gmail.com>
2022-01-24 14:23:32 +0000

    more

--- math_utils/primes.py
+++ math_utils/primes.py
@@ -4,7 +4,8 @@ def isprime(x):
     y=np.ceil(np.sqrt(x))+1
     if x==2:
         return True
-    
+    if x==1:
+        return False
     for t in range (2,int(y)):
         if (x%t) ==0:
             return False
